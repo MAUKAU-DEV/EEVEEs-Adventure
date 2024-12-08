781bb478ce5bc291d6ceed1341d2b3860d2e5ee4
Maurice Handwerker <maurice.kau@outlook.com>
2024-12-08 02:38:55 +0100

    Finished Player Controller #1
    - Player Movement done
    - PlayerController.cs needs documentation
    - PlayerCamera needs documentation
    - Player Camera smoothe follow done

--- eevees-adventure/Assets/Scripts/Player/PlayerController.cs
+++ eevees-adventure/Assets/Scripts/Player/PlayerController.cs
@@ -1,95 +1,103 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+/// <summary>
+/// The PlayerController class manages the movement inputs of the player object.
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 2.5f;
-    [SerializeField] private float _crouchSpeed = 1.0f;
-    [SerializeField] private float _jumpForce = 1.0f;
-    [SerializeField] private float _gravityScaleMuliplier = 1.0f;
-    [SerializeField] private float _gravityScaleMuliplierFalling = 1.0f;
-    [SerializeField] private float _isGroundedDistance = 1.0f;
+    [Header("Values")]
+    [SerializeField] private float _moveSpeed = 8.0f;
+    [SerializeField] private float _crouchSpeed = 4.0f;
+    [SerializeField] private float _jumpForce = 15.0f;
+    [SerializeField] private float _gravity = 16.0f;
+    [SerializeField] private float _isGroundedDistance = 1.1f;
 
+    [Header("Connected Components")]
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private Collider _collider;
     
+    // Internal
     private InputController _inputController;
-    private float _gravityScaleGrounded;
-    private float _gravityScaleFalling;
-    private float _gravityScale;
+    private Vector3 _currentMovement;
 
+    #region UNITY_RUNTIME
     private void Awake()
     {
         _inputController = new InputController();
         _inputController.PlayerGameplay.Enable();
-        _inputController.PlayerGameplay.Movement_Jump.performed += _ => Jump();
-
-
-        //this._gravityScaleGrounded = (Physics.gravity.y * (this._gravityScaleMuliplier - 1) * this._rigidbody.mass);
-        //this._gravityScaleFalling = (Physics.gravity.y * (this._gravityScaleMuliplierFalling - 1) * this._rigidbody.mass);
-
+        _inputController.PlayerGameplay.Movement_Jump.performed += Jump;
     }
 
     private void OnDestroy()
     {
-        _inputController.PlayerGameplay.Movement_Jump.performed -= _ => Jump();
+        _inputController.PlayerGameplay.Movement_Jump.performed -= Jump;
         _inputController.PlayerGameplay.Disable();
     }
 
-    private void Update()
-    {
-        //GravityModifier();
-    }
 
     private void FixedUpdate()
     {
 
-        this._gravityScaleGrounded = (Physics.gravity.y * (this._gravityScaleMuliplier - 1) * this._rigidbody.mass);
-        this._gravityScaleFalling = (Physics.gravity.y * (this._gravityScaleMuliplierFalling - 1) * this._rigidbody.mass);
-        GravityModifier();
+        SideMovement(this._inputController.PlayerGameplay.Movement_Sides.ReadValue<float>());
+        this._rigidbody.linearVelocity = this._currentMovement;
 
-        SideMovement(_inputController.PlayerGameplay.Movement_Sides.ReadValue<float>());   
+        Gravity();
     }
 
+    #endregion
+
+    /// <summary>
+    /// SideMovement moves the player to the sides on the X axis. If the crouch button is pressed it moves the player with the crouch speed.
+    /// </summary>
+    /// <param name="ctx">The CallbackContext from the input action.</param>
+    private void SideMovement(float inputValue)
+    {
+        if (!this._inputController.PlayerGameplay.Movement_Crouch.IsPressed())
+        {
+            // Normal Movement speed
+            this._currentMovement = new Vector3(-(inputValue) * this._moveSpeed, this._rigidbody.linearVelocity.y, 0f);
+        } else
+        {
+            // Crouch Movement speed
+            this._currentMovement = new Vector3(-(inputValue) * this._crouchSpeed, this._rigidbody.linearVelocity.y, 0f);
+        }
+    }
+
+    #region JUMP_MECHANIC
+
+    /// <summary>
+    /// Lets the player object jump with a upward impulse.
+    /// </summary>
+    /// <param name="ctx">The <c>CallbackContext</c> from the inputActions.</param>
+    private void Jump(InputAction.CallbackContext ctx)
+    {
+        if (!IsGrounded()) return;
+
+        this._rigidbody.AddForce(new Vector3(0f, this._jumpForce, 0f), ForceMode.Impulse);
+    }
+
+    /// <summary>
+    /// IsGrounded confirms that the player is grounded, by checking if it hits a collider in range set by _isGroundedDistance.
+    /// </summary>
+    /// <returns>  bool If the player is grounded it returns true if not then false. </returns>
     private bool IsGrounded()
     {
         return Physics.Raycast(transform.position, -Vector3.up, _isGroundedDistance);
     }
 
-    private void GravityModifier()
+    /// <summary>
+    /// Adds a own gravity to the player object to get the desired feeling.
+    /// </summary>
+    private void Gravity()
     {
-        Debug.Log(this._rigidbody.linearVelocity.y);
-
-        if (this._rigidbody.linearVelocity.y >= 0f)
+        if (this._rigidbody.linearVelocity.y < 0f)
         {
-            this._gravityScale = this._gravityScaleGrounded;
-        }
-        else if (this._rigidbody.linearVelocity.y < 0f)
-        {
-            this._gravityScale = this._gravityScaleFalling;
-        }
-
-        this._rigidbody.AddForce(new Vector3(0f, this._gravityScale, 0f), ForceMode.Force);
-    }
-
-    private void Jump()
-    {
-        if (IsGrounded())
-        {
-            _rigidbody.AddForce(new Vector3(0f, this._jumpForce, 0f), ForceMode.Force);
+            this._rigidbody.AddForce(new Vector3(0f, -(this._gravity * 2), 0f), ForceMode.Acceleration);
         } else
         {
-            Debug.Log("NOT GROUNDED");
+            this._rigidbody.AddForce(new Vector3(0f, -(this._gravity), 0f), ForceMode.Acceleration);
         }
     }
 
-    private void SideMovement(float inputValue)
-    {
-        if (!this._inputController.PlayerGameplay.Movement_Crouch.IsPressed())
-        {
-            _rigidbody.linearVelocity = new Vector3(-(inputValue) * this._moveSpeed, 0f, 0f);
-        } else
-        {
-            _rigidbody.linearVelocity = new Vector3(-(inputValue) * this._crouchSpeed, 0f, 0f);
-        }
-    }
+    #endregion
 }
